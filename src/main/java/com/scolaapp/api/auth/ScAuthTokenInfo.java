6b63d5a1b616a25ed0c582e670e3d6a14b7bce61
Anders Blehr <ablehr@gmail.com>
2012-03-23 23:39:46 +0100

    Today's work

---
+++ src/main/java/com/scolaapp/api/auth/ScAuthTokenInfo.java
@@ -0,0 +1,41 @@
+package com.scolaapp.api.auth;
+
+import java.util.Calendar;
+import java.util.Date;
+
+import javax.persistence.Id;
+
+import com.googlecode.objectify.annotation.Cached;
+import com.googlecode.objectify.annotation.Entity;
+import com.googlecode.objectify.annotation.Indexed;
+import com.googlecode.objectify.annotation.Unindexed;
+
+
+@Entity
+@Unindexed
+@Cached(expirationSeconds=600)
+public class ScAuthTokenInfo
+{
+    public @Id String authToken;
+    
+    public @Indexed String deviceId;
+    public String userId;
+    public Date dateExpires;
+    
+    
+    public ScAuthTokenInfo() {}
+    
+    
+    public ScAuthTokenInfo(String authToken, String deviceId, String userId)
+    {
+        this.authToken = authToken;
+        this.deviceId = deviceId;
+        this.userId = userId;
+        
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(new Date());
+        calendar.add(Calendar.DATE, 14);
+        
+        dateExpires = calendar.getTime();
+    }
+}
