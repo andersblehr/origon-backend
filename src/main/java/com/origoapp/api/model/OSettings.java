d001fb52ea2b8160eeeee1385461e1f052f31192
Anders Blehr <ablehr@gmail.com>
2013-05-22 17:59:26 +0200

    - Moving forward again, best idea I have is this one. Will focus on key   features for 1.0, then add features in later versions.

---
+++ src/main/java/com/origoapp/api/model/OSettings.java
@@ -0,0 +1,32 @@
+package com.origoapp.api.model;
+
+import java.util.Map;
+
+import org.codehaus.jackson.annotate.JsonIgnoreProperties;
+import org.codehaus.jackson.map.annotate.JsonSerialize;
+import org.codehaus.jackson.map.annotate.JsonSerialize.Inclusion;
+
+import com.googlecode.objectify.Key;
+import com.googlecode.objectify.annotation.Cache;
+import com.googlecode.objectify.annotation.EntitySubclass;
+import com.googlecode.objectify.annotation.IgnoreSave;
+
+
+@EntitySubclass
+@Cache(expirationSeconds = 600)
+@JsonSerialize(include = Inclusion.NON_NULL)
+@JsonIgnoreProperties(value = {"origoKey", "userKey"}, ignoreUnknown = true)
+public class OSettings extends OReplicatedEntity
+{
+    public String origoCountryCode;
+    
+    public @IgnoreSave OMember user;
+    public @IgnoreSave Map<String, String> userRef;
+    public Key<OMember> userKey;
+
+    
+    public OSettings()
+    {
+        super();
+    }
+}
