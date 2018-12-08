3d489fcef5aef0b45d23d9cc67f068ad740a3cde
Anders Blehr <anders@andersblehr.co>
2018-12-08 13:46:00 +0100

    Finalise `Config` class

--- src/main/java/co/origon/api/helpers/Config.java
+++ src/main/java/co/origon/api/helpers/Config.java
@@ -4,6 +4,10 @@ import com.googlecode.objectify.Key;
 import com.googlecode.objectify.annotation.Cache;
 import com.googlecode.objectify.annotation.Entity;
 import com.googlecode.objectify.annotation.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.HashMap;
@@ -12,27 +16,32 @@ import java.util.Map;
 import static com.googlecode.objectify.ObjectifyService.ofy;
 
 
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 @Cache(expirationSeconds = 60)
-public class Config
-{
+public class Config {
+
     public interface Category {
         String JWT = "jwt";
         String MAILER = "mailer";
-        String MAINTENANCE_MODE = "maintenanceMode";
+        String SYSTEM = "system";
     }
 
     public interface Setting {
-        // JWT settings
+        // JWT setting keys
         String EXPIRES_IN_SECONDS = "expiresInSeconds";
         String ISSUER = "issuer";
         String SECRET = "secret";
 
-        // Mailer settings
+        // Mailer setting keys
         String BASE_URL = "baseUrl";
 
-        // Maintenance mode settings
-        String ON = "on";
+        // System setting keys
+        String STATUS = "status";
+        // System settings
+        String STATUS_OK = "OK";
     }
 
     public @Id String category;
@@ -43,9 +52,8 @@ public class Config
     public static JSONObject get(String category) {
         if (!configs.containsKey(category)) {
             final Config config = ofy().load().key(Key.create(Config.class, category)).now();
-
             if (config == null) {
-                throw new RuntimeException("No configuration settings for category: " + category);
+                throw new IllegalArgumentException("No configuration settings for category: " + category);
             }
 
             configs.put(category, new JSONObject(config.configJson));
