5e3de1e1a44c865a0a84a32f20df8fa187f85bc8
Anders Blehr <anders@andersblehr.co>
2018-12-08 13:47:35 +0100

    Finalize `JwtAuthenticatedFilter`; add `ServiceAvailableFilter`

---
+++ src/main/java/co/origon/api/filters/ServiceAvailableFilter.java
@@ -0,0 +1,31 @@
+package co.origon.api.filters;
+
+import co.origon.api.helpers.Config;
+import co.origon.api.helpers.Config.Category;
+import co.origon.api.helpers.Config.Setting;
+
+import javax.annotation.Priority;
+import javax.ws.rs.ServiceUnavailableException;
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerRequestFilter;
+import javax.ws.rs.ext.Provider;
+
+@Provider
+@Priority(1)
+public class ServiceAvailableFilter implements ContainerRequestFilter {
+
+    @Override
+    public void filter(ContainerRequestContext requestContext) {
+        final String status;
+
+        try {
+            status = Config.get(Category.SYSTEM).getString(Setting.STATUS);
+        } catch (Exception e) {
+            throw new ServiceUnavailableException("System status unavailable: " + e.getMessage());
+        }
+
+        if (status == null || !status.equals(Setting.STATUS_OK)) {
+            throw new ServiceUnavailableException("Service unavailable. System status: " + status);
+        }
+    }
+}
