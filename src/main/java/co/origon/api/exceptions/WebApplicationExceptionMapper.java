514c25a67da899275e9a9f7caf5c29d888b9a0a9
Anders Blehr <anders@andersblehr.co>
2018-12-16 12:21:18 +0100

    Do not log warning when returning 404

--- src/main/java/co/origon/api/exceptions/WebApplicationExceptionMapper.java
+++ src/main/java/co/origon/api/exceptions/WebApplicationExceptionMapper.java
@@ -2,21 +2,23 @@ package co.origon.api.exceptions;
 
 import co.origon.api.OrigonApplication;
 
+import javax.servlet.http.HttpServletResponse;
 import javax.ws.rs.*;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.ext.ExceptionMapper;
 import javax.ws.rs.ext.Provider;
 import java.util.logging.Logger;
 
-
 @Provider
-public class WebApplicationExceptionMapper implements ExceptionMapper<WebApplicationException>
-{
+public class WebApplicationExceptionMapper implements ExceptionMapper<WebApplicationException> {
+
 	private static final Logger LOG = Logger.getLogger(OrigonApplication.class.getName());
 
-	public Response toResponse(WebApplicationException exception)
-	{
-		LOG.warning(exception.getMessage());
-		return exception.getResponse();
+	public Response toResponse(WebApplicationException e) {
+		if (e.getResponse().getStatus() != HttpServletResponse.SC_NOT_FOUND) {
+			LOG.warning(e.getMessage());
+		}
+
+		return e.getResponse();
 	}
 }
