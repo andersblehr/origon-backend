b48876ae70260a3155b1ddcac589031477ca677b
Anders Blehr <anders@andersblehr.co>
2018-12-30 06:05:29 +0100

    Minor code cleanup

--- src/test/java/co/origon/api/common/BasicAuthCredentialsTest.java
+++ src/test/java/co/origon/api/common/BasicAuthCredentialsTest.java
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
-import java.lang.reflect.Field;
 import java.util.Base64;
 
 import static org.junit.jupiter.api.Assertions.*;
