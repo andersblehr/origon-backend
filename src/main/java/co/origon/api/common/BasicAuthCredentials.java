2884e4b2b8182da9c36bb9c7cb92f974b8601dac
Anders Blehr <anders@andersblehr.co>
2018-12-31 09:07:11 +0100

    Properly clean up ThreadLocals between requests and on trapping exceptionos

--- src/main/java/co/origon/api/common/BasicAuthCredentials.java
+++ src/main/java/co/origon/api/common/BasicAuthCredentials.java
@@ -6,7 +6,7 @@ import java.util.Base64;
 
 @Getter
 public class BasicAuthCredentials {
-    private static ThreadLocal<BasicAuthCredentials> localCredentials = ThreadLocal.withInitial(() -> null);
+    private static ThreadLocal<BasicAuthCredentials> localCredentials;
     private static final int LENGTH_PASSWORD_MIN = 6;
 
     private final String email;
@@ -14,34 +14,33 @@ public class BasicAuthCredentials {
     private final String passwordHash;
 
     public static BasicAuthCredentials validate(String authorizationHeader) {
-        if (localCredentials.get() != null) {
+        if (localCredentials != null)
             throw new RuntimeException("Basic auth credentials have already been validated");
-        }
+
         final BasicAuthCredentials credentials = new BasicAuthCredentials(authorizationHeader);
         localCredentials = ThreadLocal.withInitial(() -> credentials);
         return localCredentials.get();
     }
 
     public static BasicAuthCredentials getCredentials() {
+        if (localCredentials == null)
+            throw new RuntimeException("No basic auth credentials have been validated, cannot get");
         return localCredentials.get();
     }
 
     public static void dispose() {
-        localCredentials = ThreadLocal.withInitial(() -> null);
+        localCredentials = null;
     }
 
     private BasicAuthCredentials(String authorizationHeader) {
-        if (authorizationHeader == null || authorizationHeader.length() == 0) {
+        if (authorizationHeader == null || authorizationHeader.length() == 0)
             throw new IllegalArgumentException("Missing Authorization header");
-        }
 
         final String[] authElements = authorizationHeader.split(" ");
-        if (authElements.length != 2) {
+        if (authElements.length != 2)
             throw new IllegalArgumentException("Invalid Authorization header: " + authorizationHeader);
-        }
-        if (!authElements[0].equals("Basic")) {
+        if (!authElements[0].equals("Basic"))
             throw new IllegalArgumentException("Invalid authentication scheme for HTTP basic auth: " + authElements[0]);
-        }
 
         final String credentialsString;
         try {
@@ -51,15 +50,12 @@ public class BasicAuthCredentials {
         }
 
         final String[] credentials = credentialsString.split(":");
-        if (credentials.length != 2) {
+        if (credentials.length != 2)
             throw new IllegalArgumentException("Invalid basic auth credentials: " + credentialsString);
-        }
-        if (!credentials[0].matches("^.+@.+\\..+$")) {
+        if (!credentials[0].matches("^.+@.+\\..+$"))
             throw new IllegalArgumentException("Invalid email address: " + credentials[0]);
-        }
-        if (credentials[1].length() < LENGTH_PASSWORD_MIN) {
+        if (credentials[1].length() < LENGTH_PASSWORD_MIN)
             throw new IllegalArgumentException("Password is too short: " + credentials[1]);
-        }
 
         email = credentials[0];
         password = credentials[1];
