50ea7886563fef6e7c7c658bee8f2445e56323e3
Anders Blehr <anders@andersblehr.co>
2018-12-31 12:40:38 +0100

    Adopt better test naming convention

--- src/test/java/co/origon/api/filter/TokenAuthenticatedFilterTest.java
+++ src/test/java/co/origon/api/filter/TokenAuthenticatedFilterTest.java
@@ -1,59 +1,58 @@
 package co.origon.api.filter;
 
+import co.origon.api.common.BasicAuthCredentials;
 import co.origon.api.common.UrlParams;
 import co.origon.api.model.api.Dao;
 import co.origon.api.model.api.DaoFactory;
 import co.origon.api.model.api.entity.DeviceCredentials;
 import co.origon.api.model.api.entity.MemberProxy;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Nested;
-import org.junit.jupiter.api.Test;
+
+import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.core.MultivaluedMap;
 import javax.ws.rs.core.UriInfo;
-
 import java.util.Date;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static co.origon.api.common.Base64.encode;
 
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.lenient;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class TokenAuthenticatedFilterTest {
 
+    private final static String VALID_AUTH_HEADER = "Basic " + encode("user@example.com:password");
     private final static String VALID_DEVICE_TOKEN = "96ae6cd160219b214ba8fe816344a478145a2a61";
     private final static String VALID_EMAIL = "user@example.com";
 
+    @Mock private DaoFactory daoFactory;
     @Mock private ContainerRequestContext requestContext;
     @Mock private UriInfo uriInfo;
     @Mock private MultivaluedMap<String, String> queryParameters;
-    @Mock private DaoFactory daoFactory;
     @Mock private Dao<DeviceCredentials> tokenCredentialsDao;
     @Mock private DeviceCredentials tokenCredentials;
     @Mock private Dao<MemberProxy> memberProxyDao;
     @Mock private MemberProxy memberProxy;
 
-    @InjectMocks
     private TokenAuthenticatedFilter tokenAuthenticatedFilter;
 
     @BeforeEach
     void setUp() {
-        tokenAuthenticatedFilter = new TokenAuthenticatedFilter();
+        BasicAuthCredentials.validate(VALID_AUTH_HEADER);
+        tokenAuthenticatedFilter = new TokenAuthenticatedFilter(daoFactory);
     }
 
     @Nested
     class Filter {
 
         @Test
-        @DisplayName("Throw no exceptions when device token is valid")
-        void throwNoExceptions_whenValidDeviceToken() {
+        @DisplayName("Given valid device token, throw run to completion")
+        void givenValidDeviceToken_thenRunToCompletion() {
             // given
             when(requestContext.getUriInfo())
                     .thenReturn(uriInfo);
@@ -84,9 +83,14 @@ class TokenAuthenticatedFilterTest {
         }
 
         @Test
-        @DisplayName("Throw BadRequestException when missing or invalid device token")
-        void throwBadRequestException_whenMissingOrInvalidDeviceToken() {
+        @DisplayName("Given invalid device token, throw BadRequestException")
+        void givenInvalidDeviceToken_thenThrowBadRequestException() {
 
         }
+    }
+
+    @AfterEach
+    void tearDown() {
+        BasicAuthCredentials.dispose();
     }
 }
