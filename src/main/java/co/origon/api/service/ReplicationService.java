3b7589490b859618408e2f866e366b23cc745fae
Anders Blehr <anders@andersblehr.co>
2020-01-26 20:28:20 +0100

    wip: Continue refactoring..

--- src/main/java/co/origon/api/service/ReplicationService.java
+++ src/main/java/co/origon/api/service/ReplicationService.java
@@ -10,15 +10,17 @@ import co.origon.api.model.ofy.entity.OReplicatedEntityRef;
 import co.origon.api.replication.Replicator;
 import co.origon.mailer.api.Mailer;
 import com.googlecode.objectify.Key;
-import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Date;
-import java.util.HashSet;
 import java.util.List;
+import java.util.Optional;
 import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import javax.inject.Inject;
 
 public class ReplicationService {
-  private Set<Key<OReplicatedEntity>> referencedEntityKeys;
+
+  @Inject private Mailer mailer;
 
   public void replicate(List<OReplicatedEntity> entities, String userEmail, Mailer mailer) {
     if (entities.size() > 0) {
@@ -30,94 +32,60 @@ public class ReplicationService {
     return fetch(userEmail, null);
   }
 
-  public List<OReplicatedEntity> fetch(String userEmail, Date deviceReplicationDate) {
-    referencedEntityKeys = new HashSet<>();
-
-    OMemberProxy memberProxy = OMemberProxy.get(userEmail);
-    Collection<OMembership> memberships = ofy().load().keys(memberProxy.membershipKeys()).values();
-
-    Set<OReplicatedEntity> fetchedEntities = new HashSet<>();
-
-    for (OMembership membership : memberships) {
-      if (membership.isFetchable()) {
-        if (deviceReplicationDate == null || membership.dateCreated.after(deviceReplicationDate)) {
-          fetchedEntities.addAll(fetchMembershipEntities(membership, null));
-        } else {
-          fetchedEntities.addAll(fetchMembershipEntities(membership, deviceReplicationDate));
-        }
-      } else if (membership.isExpired
-          && (deviceReplicationDate == null
-              || membership.dateReplicated.after(deviceReplicationDate))) {
-        fetchedEntities.add(membership);
-      }
-    }
-
-    if (referencedEntityKeys.size() > 0) {
-      fetchedEntities.addAll(ofy().load().keys(referencedEntityKeys).values());
-    }
-
-    return new ArrayList<>(fetchedEntities);
+  public List<OReplicatedEntity> fetch(String userEmail, Date deviceReplicatedAt) {
+    return ofy().load().keys(OMemberProxy.get(userEmail).membershipKeys()).values().stream()
+        .flatMap(
+            membership ->
+                membership.isFetchable()
+                    ? fetchMembershipEntities(membership, deviceReplicatedAt)
+                    : membership.isExpired ? Stream.of(membership) : Stream.of())
+        .distinct()
+        .collect(Collectors.toList());
   }
 
   public List<OReplicatedEntity> lookupMember(String memberId) {
-    Set<OReplicatedEntity> memberEntities = new HashSet<>();
-    OMemberProxy memberProxy = ofy().load().key(Key.create(OMemberProxy.class, memberId)).now();
-
-    if (memberProxy != null) {
-      referencedEntityKeys = new HashSet<>();
-
-      for (OMembership membership : ofy().load().keys(memberProxy.membershipKeys()).values()) {
-        if (membership.type.equals("R") && !membership.isExpired) {
-          memberEntities.addAll(fetchMembershipEntities(membership, null));
-        }
-      }
-
-      if (referencedEntityKeys.size() > 0) {
-        memberEntities.addAll(ofy().load().keys(referencedEntityKeys).values());
-      }
-    }
-
-    return memberEntities.size() > 0 ? new ArrayList<>(memberEntities) : null;
+    return Optional.ofNullable(ofy().load().key(Key.create(OMemberProxy.class, memberId)).now())
+        .map(
+            proxy ->
+                ofy().load().keys(proxy.membershipKeys()).values().stream()
+                    .filter(membership -> membership.type.equals("R") && !membership.isExpired)
+                    .flatMap(membership -> fetchMembershipEntities(membership, null))
+                    .distinct()
+                    .collect(Collectors.toList()))
+        .orElse(null);
   }
 
   public OOrigo lookupOrigo(String internalJoinCode) {
-    OReplicatedEntity origo =
+    return (OOrigo)
         ofy()
             .load()
             .type(OReplicatedEntity.class)
             .filter("internalJoinCode", internalJoinCode)
             .first()
             .now();
-
-    return (OOrigo) origo;
   }
 
-  private Set<OReplicatedEntity> fetchMembershipEntities(
-      OMembership membership, Date deviceReplicationDate) {
-    List<OReplicatedEntity> fetchedEntities;
-    Set<OReplicatedEntity> membershipEntities = new HashSet<>();
+  private Stream<OReplicatedEntity> fetchMembershipEntities(
+      OMembership membership, Date deviceReplicatedAt) {
+    List<OReplicatedEntity> membershipEntities =
+        deviceReplicatedAt != null
+            ? ofy()
+                .load()
+                .type(OReplicatedEntity.class)
+                .ancestor(membership.parentKey)
+                .filter("dateReplicated >", deviceReplicatedAt)
+                .list()
+            : ofy().load().type(OReplicatedEntity.class).ancestor(membership.parentKey).list();
 
-    if (deviceReplicationDate != null) {
-      fetchedEntities =
-          ofy()
-              .load()
-              .type(OReplicatedEntity.class)
-              .ancestor(membership.parentKey)
-              .filter("dateReplicated >", deviceReplicationDate)
-              .list();
-    } else {
-      fetchedEntities =
-          ofy().load().type(OReplicatedEntity.class).ancestor(membership.parentKey).list();
-    }
-
-    for (OReplicatedEntity entity : fetchedEntities) {
-      if (entity.getClass().equals(OReplicatedEntityRef.class)) {
-        referencedEntityKeys.add(((OReplicatedEntityRef) entity).referencedEntityKey);
-      }
-
-      membershipEntities.add(entity);
-    }
-
-    return membershipEntities;
+    return Stream.concat(
+            membershipEntities.stream(),
+            ofy().load()
+                .keys(
+                    membershipEntities.stream()
+                        .filter(entity -> entity.getClass().equals(OReplicatedEntityRef.class))
+                        .map(entity -> (OReplicatedEntityRef) entity)
+                        .map(entityRef -> entityRef.referencedEntityKey)
+                        .collect(Collectors.toSet()))
+                .values().stream());
   }
 }
